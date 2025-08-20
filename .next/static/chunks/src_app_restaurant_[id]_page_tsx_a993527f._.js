07166a8d1b08808f0ab328d435789048a6c61bbb
Leander <108235898+Leander015@users.noreply.github.com>
2025-08-20 13:09:39 -0300

    Add .next/static/chunks/src_app_restaurant_[id]_page_tsx_a993527f._.js 1755706180408

---
+++ .next/static/chunks/src_app_restaurant_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_18ef916b._.js",
+  "static/chunks/node_modules_b52775e3._.js"
+],
+    source: "dynamic"
+});
